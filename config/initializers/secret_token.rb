496d072441cb555bbdadeb22cae60bb909ee1667
Tejas Ranade <tejas.ranade@gmail.com>
2012-07-10 20:57:20 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tejas::Application.config.secret_token = 'baf9b8093cf22b94aa86d256eb8586ff69992218f892b15fc23eef1526ebbcf8a32dd24d10f77e645a1b4c86d7a56eee7b8bc43792f67113540ef2830e9661f1'
